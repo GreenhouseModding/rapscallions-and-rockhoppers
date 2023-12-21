484b251511ff187a68429f026d166a3094fb5373
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-22 05:30:08 +1100

    Fix stroll goal returning poorly on land.

--- common/src/main/java/dev/greenhouseteam/rapscallionsandrockhoppers/entity/goal/PenguinStrollGoal.java
+++ common/src/main/java/dev/greenhouseteam/rapscallionsandrockhoppers/entity/goal/PenguinStrollGoal.java
@@ -3,6 +3,7 @@ package dev.greenhouseteam.rapscallionsandrockhoppers.entity.goal;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.behavior.BehaviorUtils;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.util.LandRandomPos;
 import net.minecraft.world.phys.Vec3;
 
 public class PenguinStrollGoal extends RandomStrollGoal {
@@ -12,6 +13,6 @@ public class PenguinStrollGoal extends RandomStrollGoal {
 
     @Override
     public Vec3 getPosition() {
-        return this.mob.isInWaterOrBubble() ? BehaviorUtils.getRandomSwimmablePos(this.mob, 10, 7) : super.getPosition();
+        return this.mob.isInWaterOrBubble() ? BehaviorUtils.getRandomSwimmablePos(this.mob, 10, 7) : LandRandomPos.getPos(this.mob, 10, 7);
     }
 }
