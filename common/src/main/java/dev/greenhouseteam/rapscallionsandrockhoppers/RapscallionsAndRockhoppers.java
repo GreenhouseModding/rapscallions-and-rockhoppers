47b56138e77f36f78c2dd09de2be2812c7fbed33
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-24 14:06:02 +1100

    Remove unnecessary imports.

--- common/src/main/java/dev/greenhouseteam/rapscallionsandrockhoppers/RapscallionsAndRockhoppers.java
+++ common/src/main/java/dev/greenhouseteam/rapscallionsandrockhoppers/RapscallionsAndRockhoppers.java
@@ -1,19 +1,8 @@
 package dev.greenhouseteam.rapscallionsandrockhoppers;
 
-import dev.greenhouseteam.rapscallionsandrockhoppers.entity.Penguin;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.animal.Cow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.function.BiConsumer;
-import java.util.function.Supplier;
 
 public class RapscallionsAndRockhoppers {
 
