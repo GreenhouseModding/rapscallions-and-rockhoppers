84708f1cc99f7275399b6a5df1f4e3f3534b423b
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-30 13:59:14 +1100

    Swap to lower weight.

--- fabric/src/main/java/dev/greenhouseteam/rapscallionsandrockhoppers/RapscallionsAndRockhoppersFabric.java
+++ fabric/src/main/java/dev/greenhouseteam/rapscallionsandrockhoppers/RapscallionsAndRockhoppersFabric.java
@@ -50,9 +50,9 @@ public class RapscallionsAndRockhoppersFabric implements ModInitializer {
     }
 
     public static void handleBiomeModifications() {
-        SpawnPlacements.register(RockhoppersEntityTypes.PENGUIN, SpawnPlacements.Type.IN_WATER, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Penguin::checkPenguinSpawnRules);
+        SpawnPlacements.register(RockhoppersEntityTypes.PENGUIN, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Penguin::checkPenguinSpawnRules);
         createBiomeModifications(RapscallionsAndRockhoppers.asResource("penguin"),
-                createPenguinSpawnPredicate(), RockhoppersEntityTypes.PENGUIN, 40, 2, 4);
+                createPenguinSpawnPredicate(), RockhoppersEntityTypes.PENGUIN, 2, 2, 4);
     }
 
     public static void createBiomeModifications(ResourceLocation location, Predicate<BiomeSelectionContext> predicate, EntityType<?> entityType, int weight, int min, int max) {
