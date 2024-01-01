cdeea88c64c96fe57ce4e83ec5ceb133e527bae0
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-01-01 21:21:33 +1100

    Fix penguins barely moving on ice.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,4 +6,5 @@
 - Fixed crash that happens when a penguin has a boat to follow.
 - Fixed boats being able to be sunk underwater with a boat hook.
 - Fixed an infinite height exploit involving a boat hook.
-- Fix boat hook item not dropping upon breaking a boat when attached to a player.
+- Fixed boat hook item not dropping upon breaking a boat when attached to a player.
+- Fixed penguins barely moving on ice.
--- common/src/main/java/dev/greenhouseteam/rapscallionsandrockhoppers/mixin/LivingEntityMixin.java
+++ common/src/main/java/dev/greenhouseteam/rapscallionsandrockhoppers/mixin/LivingEntityMixin.java
@@ -2,11 +2,10 @@ package dev.greenhouseteam.rapscallionsandrockhoppers.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.greenhouseteam.rapscallionsandrockhoppers.entity.Penguin;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.MobType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,10 +15,13 @@ public abstract class LivingEntityMixin {
 
     @ModifyReturnValue(method = "getFrictionInfluencedSpeed", at = @At("RETURN"))
     private float rapscallionsandrockhoppers$modifyPenguinFrictionWhenStumble(float original) {
-        if (((LivingEntity)(Object)this instanceof Penguin penguin) && penguin.isStumbling()) {
-            return original * 1.8F;
+        if (((LivingEntity)(Object)this instanceof Penguin penguin)) {
+            if (penguin.isStumbling()) {
+                return original * 1.8F;
+            } else if (penguin.getBlockStateOn().is(BlockTags.ICE)) {
+                return original * 1.2F;
+            }
         }
-
         return original;
     }
 
